1d53984552b0951a2843d60c9e61cb91a36c26c1
David Adebomi <140355207+bigspesh@users.noreply.github.com>
2024-08-18 23:01:05 +0100

    second set of revisons

--- revisions1.py
+++ revisions1.py
@@ -17,3 +17,34 @@ print(newvalues1)
 
 my_tuple = (1,2,3,4,5)
 print(list(my_tuple))
+
+# s = "SIMPLILEARN"
+# for i in s:
+#     print(i, end="")
+
+list2 = ['ctrl', 'John', "David"]
+for i in range(len(list2)):
+    print(f"Hello:{list2[i]}")
+
+
+a = "aaa,bbb,ccc"
+b = Counter(a)
+print(list(a))
+
+
+cities_in_F = {'New York': 32, 'Boston': 55, 'Denver': 65, 'Los_Angeles': 34}
+a = Counter(cities_in_F)
+print(a)
+
+
+user_input = (input("Enter Change:"))
+print("HELLO" + user_input)
+
+
+car = 4.2
+print("this is %d" %car )
+
+text = input()
+for i in text.split():
+    print(i)
+
