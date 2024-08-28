ec2c1433d34a1c41ffcba824f0f5c3f91c76f4b8
David Adebomi <140355207+bigspesh@users.noreply.github.com>
2024-08-28 23:44:58 +0100

    Adding my 2nd commits

--- revisions1.py
+++ revisions1.py
@@ -111,16 +111,7 @@ def course_count_prompt():
         except ValueError:
             print("Invalid input. Enter a number between 5 and 12.")
 
-def course_count_prompt():
-    while True:
-        try:
-            course_count = int(input("Enter the number of courses you want to study (5-12): "))
-            if 5 <= course_count <= 12:
-                return course_count
-            else:
-                print("Course count should be between 5 and 12.")
-        except ValueError:
-            print("Invalid input. Enter a number between 5 and 12.")
+
 
 # in this case an unpacking is also going on here, but this time the course_count_prompt() function is not 
 # returning a tuple but rather returning the user_input which is course_count, then after you assign it to the variable course_count again
